d8a068f151f1fd55cdfe4b104b7fa02dcaa21078
Marinna Damaso <101846310+maridamaso@users.noreply.github.com>
2024-06-04 08:31:37 -0300

    Add files via upload

--- Restaurante/back-end/server.js
+++ Restaurante/back-end/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const multer = require('multer');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' })); // Aumente o limite para 10MB, se necessário
